61315204104d0c9f8819adb250d84dd64cf6fa42
dracu <dracula337435@163.com>
2020-02-25 15:21:26 +0800

    注册中心改为multicast

--- src/test/java/org/dracula/test/dubbo/test4/consumer/ConsumerTest.java
+++ src/test/java/org/dracula/test/dubbo/test4/consumer/ConsumerTest.java
@@ -38,7 +38,7 @@ public class ConsumerTest {
         @Bean
         public RegistryConfig registryConfig(){
             RegistryConfig registryConfig = new RegistryConfig();
-            registryConfig.setAddress("zookeeper://127.0.0.1:2181");
+            registryConfig.setAddress("multicast://224.5.6.7:1234");
             return registryConfig;
         }
 
